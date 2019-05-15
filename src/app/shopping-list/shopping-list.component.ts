d8625601680745fe9a2b0338cf81f0763e3bf175
amit <amit10987@gmail.com>
2019-05-15 23:33:00 +0400

    initial commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector: "app-shopping-list",
+    templateUrl: "./shopping-list.component.html",
+    styleUrls: [
+        "./shopping-list.component.css"
+    ]
+})
+export class ShoppingListComponent{
+
+}
